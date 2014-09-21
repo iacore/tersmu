8646e26a27e4d7a191e09878c5ec2a1af643eaf7
Martin Bays <mbays@sdf.lonestar.org>
2014-09-21 17:47:05 -0400

    irc: identify with nickserv

--- tersmuBot.hs
+++ tersmuBot.hs
@@ -3,55 +3,78 @@
 -- http://www.wastedartist.com/scripts/daskeb/daskeb.hs
 import Network
 import System.IO
+import System.IO.Error
 import Text.Printf
 import Data.List
 import System.Exit
 import Text.Regex.Posix
+import System.Timeout
+import Control.Monad.Trans.State
+import Control.Monad.IO.Class
+import Control.Applicative
+import Control.Concurrent (threadDelay)
 
 import TersmuIRC
 
 -- import Debug.Trace
 -- traceIt x = traceShow x x
  
-server = "morgan.freenode.net"
---server = "irc.freenode.org"
+--server = "morgan.freenode.net"
+server = "irc.freenode.org"
 port   = 6667
 chan   = "#lojban"
 nick   = "tersmus"
+timeoutTime = 200
  
-main = do
+main = connectToServer >>= listen
+
+connectToServer = do
     h <- connectTo server (PortNumber (fromIntegral port))
     hSetBuffering h NoBuffering
-    write h "NICK" nick
-    write h "USER" (nick++" 0 * :digitshaskellbot")
-    write h "JOIN" chan
-    listen h
+    password <- readFile "ircPassword"
+    write "USER" ((nick++" 0 * :tersmuBot")) h
+    write "NICK" (nick++"_") h
+    write "PRIVMSG" ("NickServ :regain "++nick++" "++password) h
+    write "PRIVMSG" ("NickServ :identify "++nick++" "++password) h
+    write "JOIN" chan h
+    return h
  
-write :: Handle -> String -> String -> IO ()
-write h s t = do
+write :: String -> String -> Handle -> IO ()
+write s t h = do
     hPrintf h "%s %s\r\n" s t
     printf    "> %s %s\n" s t
+
+reconnectingOnError :: (Handle -> IO a) -> StateT Handle IO a
+reconnectingOnError m = do
+    h <- get
+    mret <- liftIO $ (`catchIOError` (const $ return Nothing)) $ timeout (timeoutTime * 10^6) $ m h
+    case mret of
+	Nothing -> do
+	    liftIO $ threadDelay $ 10 * 10^6
+	    h <- liftIO $ connectToServer
+	    put h
+	    reconnectingOnError m
+	Just ret -> return ret
  
 listen :: Handle -> IO ()
-listen h = forever $ do
-    t <- hGetLine h
-    let s = init t
-    putStrLn s
+listen h = flip evalStateT h $ forever $ do
+    s <- init <$> reconnectingOnError hGetLine
+    liftIO $ putStrLn s
     if ping s then pong s else eval s
   where
     forever a = a >> forever a
- 
+
     clean     = drop 1 . dropWhile (/= ':') . drop 1
  
     ping x    = "PING :" `isPrefixOf` x
-    pong x    = write h "PONG" (':' : drop 6 x)
+    pong x    = reconnectingOnError $ write "PONG" (':' : drop 6 x)
 
-    privmsg :: String -> String -> IO ()
-    privmsg to s = write h "PRIVMSG" (to ++ " :" ++ s)
+    privmsg :: String -> String -> StateT Handle IO ()
+    privmsg to s = reconnectingOnError $ write "PRIVMSG" (to ++ " :" ++ s)
 
     chanmsg = privmsg chan
     
-    eval :: String -> IO ()
+    eval :: String -> StateT Handle IO ()
     eval s = case s =~ ":([^!]+)!([^ ]+) PRIVMSG ([^ ]+) :(.*)" of
 	[[_,user,_,to,msg]] -> let
 		isPrivate = to == nick
