87b3b42dceacfe1191c43da9a6128c271c569921
Martin Bays <mbays@sdf.lonestar.org>
2014-09-25 21:23:22 -0400

    add GPL headers

--- JboShow.hs
+++ JboShow.hs
@@ -1,3 +1,13 @@
+-- This file is part of tersmu
+-- Copyright (C) 2014 Martin Bays <mbays@sdf.org>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of version 3 of the GNU General Public License as
+-- published by the Free Software Foundation.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see http://www.gnu.org/licenses/.
+
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 module JboShow where
 
